520e73460dac3e0d4f883a1d1f0be998217b24e2
riv <riverosmejiamiguel@gmail.com>
2023-10-24 15:16:12 -0500

    inicio del trabajo en python

---
+++ ConsoleApplication.py
@@ -0,0 +1,8 @@
+import execute
+
+def main():
+    ejecutar = execute.execute()
+    ejecutar.arranque()
+
+if __name__ == "__main__":
+    main()
---
+++ execute.cpp
@@ -0,0 +1,154 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+
+#include "Car.h"  // Incluye el archivo de encabezado de Car
+#include "Lista.h"  // Incluye el archivo de encabezado de List
+
+using namespace std;
+
+class excecute {
+public:
+
+    void arranque() {
+
+        Lista ListaAutos;
+
+        int serie=1;
+
+        Car carro1(serie, "Renault", "12", 1996, 20.0, false);
+        ListaAutos.agregarAuto(carro1);
+
+        serie++;
+
+        Car carro2(serie, "Fiat", "Palio", 2005, 45.0, false);
+        ListaAutos.agregarAuto(carro2);
+
+        serie++;
+
+        Car carro3(serie, "Chevrolet", "Spark", 2015, 65.0, false);
+        ListaAutos.agregarAuto(carro3);
+
+        serie++;
+
+        Car carro4(serie, "Toyota", "Corolla", 2023, 150.0, false);
+        ListaAutos.agregarAuto(carro4);
+
+        programa(ListaAutos);
+
+    }
+
+    void programa(Lista ListaAutos) {
+
+        int num;
+
+    while(num!=5){
+
+                do {
+            
+                system("cls");
+
+                cout << "MENU:\n\n1.Autos Disponibles.\n2.Alquilar Auto\n3.Devolver Auto\n4.Ingresos Totales Actuales\n5.Salir\n\nR/=";
+                cin >> num;
+
+                if (cin.fail() || num < 1 || num > 5) {
+            
+                    cout << "Número no válido. Debe estar en el rango de 1 a 5." << endl;
+            
+                    cin.clear();  // Restablecer el estado de cin
+            
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Descartar la entrada incorrecta
+                }
+            
+            } while (cin.fail() || num < 1 || num > 5);
+
+            system("cls");
+
+            if(num==1){
+
+                ListaAutos.impAuto();
+            
+            }
+
+            if(num==2){
+
+                int num1;
+
+                cout<<"dame el numero del automovil: ";
+
+                while (!(cin >> num1)) {
+                    
+                    cin.clear();  // Restablecer el estado de cin
+                    
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Descartar la entrada incorrecta
+                    
+                    cout << "Entrada invalida. Por favor, ingresa un numero: ";
+                }
+
+                if(ListaAutos.alquilarAuto(num1)){
+
+                    cout<<"\n\n<----Auto Alquilado con exito---->\n\n";
+
+                }
+                
+                else{
+
+                    cout<<"\n\n<---El numero del auto que ingresaste es invalido o ya esta alquilado--->\n\n";
+
+                }
+
+            }
+
+            if(num==3){
+
+                int num1;
+
+                cout<<"dame el numero del automovil a devolver: ";
+
+                while (!(cin >> num1)) {
+                    
+                    cin.clear();  // Restablecer el estado de cin
+                    
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Descartar la entrada incorrecta
+                    
+                    cout << "Entrada invalida. Por favor, ingresa un numero: ";
+                }
+
+                if(ListaAutos.devolverAuto(num1)){
+
+                    cout<<"\n\n<----Auto Devuelto con exito---->\n\n";
+
+                }
+                
+                else{
+
+                    cout<<"\n\n<---El numero del auto que ingresaste es invalido o ya se ha devuelto--->\n\n";
+
+                }
+
+            }
+
+            if(num==4){
+
+                double g=ListaAutos.Ganancias();
+
+                cout<<"\n\nINGRESOS ACTUALES: "<<g<<"\n\n";
+
+            }
+
+            if(num==5){
+
+                cout<<"\n\nAdios\n\n";
+
+            }
+
+            system("pause");
+
+        }
+
+        system("cls");
+
+    }
+
+};
